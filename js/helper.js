aa7365e29b0f66d47244f20acc5e5985cf71d858
CalvinH-Dev <hanisch.calvin@gmail.com>
2025-07-14 14:22:50 +0200

    Header verkleinert auf Mobile, außerdem noch ein Media Query für die Breite des Screens erstellt um die Große Karte darauf basierend anders zu stylen.

--- js/helper.js
+++ js/helper.js
@@ -300,10 +300,10 @@ function calcWeaknessesForType(type1, type2) {
 }
 
 function calcStrengthsForType(type1, type2) {
-	const weak1 = ATTACKING[type1].weak;
-	const weak2 = ATTACKING[type2].weak;
-	const strong1 = ATTACKING[type1].strong;
-	const strong2 = ATTACKING[type2].strong;
+	const weak1 = DEFENDING[type1].weak;
+	const weak2 = DEFENDING[type2].weak;
+	const strong1 = DEFENDING[type1].strong;
+	const strong2 = DEFENDING[type2].strong;
 
 	const strongList1 = strong1.difference(weak2);
 	const strongList2 = strong2.difference(weak1);
